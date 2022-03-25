2827ee9122fc9c60fab6a0fa5eb199b04dedc736
vvitoss <orangekaramel@gmail.com>
2022-03-25 17:38:15 +0300

    initial commit

---
+++ src/test/java/ru/netology/test/PostmanTest.java
@@ -0,0 +1,27 @@
+package ru.netology.test;
+
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanTest {
+
+    @Test
+    void test1 () {
+        // Given - When - Then
+// Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("some data"))
+        ;
+    }
+}
